85a17cd0859594cb2994c089616850c085575277
xcy960815 <18763006837@163.com>
2025-12-05 21:15:26 +0800

    chore: ignore temp directory and update build script

--- temp/index.d.ts
+++
@@ -1,5 +0,0 @@
-import GptModel from "./gpt-model";
-import TextModle from "./text-model";
-import { ChatgptError, Core } from "./core";
-export { GptModel, TextModle, ChatgptError, Core };
-export * from "./types";
